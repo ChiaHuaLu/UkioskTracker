980e6ade1f58b7f1add34526fa263d945664b467
Chia Hua Lu <thegoldflute@users.noreply.github.com>
2017-07-27 10:36:56 -0500

    Main Tabs Update
    MainActivity hooked up to the 3 tabs' xml and java files

--- app/src/main/java/chiahua/ukiosktracker/MainActivity.java
+++ app/src/main/java/chiahua/ukiosktracker/MainActivity.java
@@ -89,40 +89,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.kiosk_tab, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -136,15 +102,24 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    KioskTab kioskTab = new KioskTab();
+                    return kioskTab;
+                case 1:
+                    NearbyTab nearbyTab = new NearbyTab();
+                    return nearbyTab;
+                case 2:
+                    PosterTab posterTab = new PosterTab();
+                    return posterTab;
+            }
+            return null;
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 2;
+            return 3;
         }
 
         @Override
@@ -153,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
                 case 0:
                     return "Kiosks";
                 case 1:
-                    return "Posters";
+                    return "Nearby";
                 case 2:
                     return "Posters";
             }
