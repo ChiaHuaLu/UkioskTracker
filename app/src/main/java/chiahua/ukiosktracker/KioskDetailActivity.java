a4a55f71fb286a3cddff1c87dab22b24815bd393
Daniel Zheng <daniel.c.zheng@gmail.com>
2017-08-01 14:53:31 -0500

    small changes
    prepared code for nearby

--- app/src/main/java/chiahua/ukiosktracker/KioskDetailActivity.java
+++ app/src/main/java/chiahua/ukiosktracker/KioskDetailActivity.java
@@ -34,7 +34,7 @@ public class KioskDetailActivity extends AppCompatActivity {
         kioskImages.recycle();
         Log.d(TAG, "Kiosk " + kioskID + " is named " + kiosk.name());
         List<KioskPoster> allKPs = KioskPoster.listAll(KioskPoster.class);
-        ArrayList<Poster> relevantPosters = new ArrayList<Poster>();
+        ArrayList<Poster> relevantPosters = new ArrayList<>();
         for (KioskPoster kp : allKPs) {
             if (kp.matchKiosk(kiosk)) {
                 relevantPosters.add(kp.getPoster());
